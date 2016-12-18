8909c38983e8eb40d579f9b783ca6005bdcfd989
halafi <filiphalas74@gmail.com>
2016-12-18 11:16:55 +0100

    react-mixin

--- app/scripts/components/App.js
+++ app/scripts/components/App.js
@@ -1,18 +1,50 @@
 import React from "react"
-import { Container, Button, Row, Col } from "reactstrap"
+import { Container, Button, Row, Col, InputGroup, Input, InputGroupAddon } from "reactstrap"
 import { connect } from "react-redux"
-import { ReactFireMixin } from "reactfire"
+import reactMixin from "react-mixin"
+import ReactFireMixin from "reactfire"
+import firebase from "firebase"
 
 import AppNavbar from "./AppNavbar.js"
 import { incrementCredits } from "../actions/GameActions"
 import { gameSelector } from "../stores/selectors/gameSelector"
 
-@ReactFireMixin
+// Initialize Firebase
+const firebaseConf = {
+	apiKey: "AIzaSyDcvzEJoJb1w_qJcC_fEO3Xa7e_HNCreD8",
+	authDomain: "dopamine-simulator.firebaseapp.com",
+	databaseURL: "https://dopamine-simulator.firebaseio.com",
+	storageBucket: "dopamine-simulator.appspot.com",
+	messagingSenderId: "797610196288",
+}
+firebase.initializeApp(firebaseConf)
+
 class App extends React.Component {
 	constructor(props) {
+
 		super(props)
 		this.handleClick = this.handleClick.bind(this)
+		this.componentWillMount = this.componentWillMount.bind(this)
+		console.log(this)
+		this.state = {
+			text: "fap",
+		}
 	}
+
+	componentWillMount() { // run once before rednering
+		const ref = firebase.database().ref("items")
+		this.bindAsArray(ref, "items")
+		console.log(this.state)
+	}
+
+	handleSubmit(e) {
+		e.preventDefault()
+		this.firebaseRefs.items.push({
+			text: this.state.text,
+		})
+		this.setState({ text: "" })
+	}
+
 	handleClick() {
 		this.props.dispatch(incrementCredits(10))
 	}
@@ -25,7 +57,11 @@ class App extends React.Component {
 				<AppNavbar game={game} />
 				<Row>
 					<Col xs="12">
-						<Button bsStyle="primary" onClick={this.handleClick}>Bake</Button>
+						<InputGroup>
+							<InputGroupAddon>@</InputGroupAddon>
+							<Input placeholder="text" value={this.state.text} />
+						</InputGroup>
+						<Button color="primary" onClick={this.handleSubmit}>Submit</Button>
 					</Col>
 				</Row>
 			</Container>
@@ -37,6 +73,8 @@ App.propTypes = {
 	game: React.PropTypes.object,
 	dispatch: React.PropTypes.func,
 }
+
+reactMixin(App.prototype, ReactFireMixin)
 
 export default connect((state) => {
 	return {
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,20 +5,10 @@ import ReactDOM from "react-dom"
 import { Router, Route, hashHistory } from "react-router"
 import immutable from "immutable"
 import installDevTools from "immutable-devtools"
-import firebase from "firebase"
 
 import App from "./components/App.js"
 import reducers from "./stores/reducers"
 
-// Initialize Firebase
-const firebaseConf = {
-	apiKey: "AIzaSyDcvzEJoJb1w_qJcC_fEO3Xa7e_HNCreD8",
-	authDomain: "dopamine-simulator.firebaseapp.com",
-	databaseURL: "https://dopamine-simulator.firebaseio.com",
-	storageBucket: "dopamine-simulator.appspot.com",
-	messagingSenderId: "797610196288",
-}
-firebase.initializeApp(firebaseConf)
 
 // Install immutable chrome dev tools
 installDevTools(immutable)
